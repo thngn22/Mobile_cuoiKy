4789750dbe7e534e07e8c2eacf8a30f32b0c0b9f
Vu Khang <vunguyentrungkhang@gmail.com>
2023-05-10 01:13:55 +0700

    Thêm Home Activity

---
+++ app/src/main/java/vn/iotstar/nongsan/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package vn.iotstar.nongsan.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.iotstar.nongsan.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/vn/iotstar/nongsan/activities/MainActivity.java
+++ app/src/main/java/vn/iotstar/nongsan/activities/MainActivity.java
@@ -1,18 +1,24 @@
-package vn.iotstar.nongsan;
+package vn.iotstar.nongsan.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import vn.iotstar.nongsan.R;
+import vn.iotstar.nongsan.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.start
     }
 }
