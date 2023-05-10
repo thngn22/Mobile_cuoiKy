9dc1edbd8db918d57995e32677896b06fa86b86d
Vu Khang <vunguyentrungkhang@gmail.com>
2023-05-10 16:24:59 +0700

    Template Product and Cart

--- app/src/main/java/vn/iotstar/nongsan/activities/ProductActivity.java
+++ app/src/main/java/vn/iotstar/nongsan/activities/ProductActivity.java
@@ -1,2 +1,16 @@
-package vn.iotstar.nongsan.activities;public class ProductActivity {
+package vn.iotstar.nongsan.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.iotstar.nongsan.R;
+
+public class ProductActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
 }
